51628dec753ed36dce15d80a1ecdd57d494e0f4e
atish2802 <147652209+atish2802@users.noreply.github.com>
2025-01-16 11:23:44 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+session_start();
+include('./db_connect.php');
+ob_start();
+if(!isset($_SESSION['system'])){
+	$system = $conn->query("SELECT * FROM system_settings limit 1")->fetch_array();
+	foreach($system as $k => $v){
+		$_SESSION['system'][$k] = $v;
+	}
+}
+ob_end_flush();
+?>
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title><?php echo $_SESSION['system']['name'] ?></title>
+ 	
+
+<?php include('./header.php'); ?>
+<?php 
+if(isset($_SESSION['login_id']))
+header("location:index.php?page=home");
+
+?>
+
+</head>
+
+<style>
+	body{
+		margin: 20px;
+		text-align: center;
+		width: 100%;
+	    height: calc(100%);
+	    position: fixed;
+	    top:0;
+	    left: 0
+	}
+	main#main{
+		width:100%;
+		height: calc(100%);
+		display: flex;
+	}
+	h2{
+		color:blue;
+		text-align: center;
+            text-align: center;
+            font-size: 35px;
+	} 
+        img {
+			
+            float: left;
+        }
+ 
+</style>
+<img  src="logo.png" alt="collage" width="180" height="180">
+<h2>Smt. Devkiba Mohansinhji Chauhan College of Commerce & Science, Silvassa<br>Affiliated to the University of Mumbai<br></h2>
+<body class="bg-white">
+
+
+  <main id="main" >
+  		<div class="align-center w-100">
+		<h4 class="text-black text-center"><b><?php echo $_SESSION['system']['name'] ?></b></h4>
+  		<div id="login-center" class="bg-white  row justify-content-center">
+  			<div class="card col-md-4">
+  				<div class="card-body">
+  					<form id="login-form" >
+  						<div class="form-group">
+  							<label for="username" class="control-label">Username</label>
+  							<input type="text" id="username" name="username" class="form-control">
+  						</div>
+  						<div class="form-group">
+  							<label for="password" class="control-label">Password</label>
+  							<input type="password" id="password" name="password" class="form-control">
+  						</div>
+  						<center><button class="btn-sm btn-block btn-wave col-md-4 btn-primary">Login</button>
+						</center>
+  					</form>
+  				</div>
+  			</div>
+  		</div>
+  		</div>
+  </main>
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+
+</body>
+<script>
+	$('#login-form').submit(function(e){
+		e.preventDefault()
+		$('#login-form button[type="button"]').attr('disabled',true).html('Logging in...');
+		if($(this).find('.alert-danger').length > 0 )
+			$(this).find('.alert-danger').remove();
+		$.ajax({
+			url:'ajax.php?action=login',
+			method:'POST',
+			data:$(this).serialize(),
+			error:err=>{
+				console.log(err)
+		$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
+
+			},
+			success:function(resp){
+				if(resp == 1){
+					location.href ='index.php?page=home';
+				}else{
+					$('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
+					$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
+				}
+			}
+		})
+	})
+</script>	
+</html>
